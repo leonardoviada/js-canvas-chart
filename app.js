f58449fe50bdf68730e9be66dfdf9f414bad2912
leonardoviada <leo@revodigital.it>
2020-11-30 09:33:10 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,117 @@
+const m = document.getElementById('m');
+const q = document.getElementById('q');
+
+const btnCarica = document.getElementById('btnCarica');
+const btnSalva = document.getElementById('btnSalva');
+
+const ss = window.sessionStorage;
+
+const validaCampi = () => {
+  const erroreM = document.getElementById('erroreM');
+  const erroreQ = document.getElementById('erroreQ');
+
+  let ret = 0;
+
+  console.log('valido...');
+
+  if (!m.value) {
+    m.classList.add('is-invalid');
+    erroreM.innerText = 'Compila questo campo';
+    ret--;
+  } else {
+    m.classList.remove('is-invalid');
+    erroreM.innerText = '';
+    ret++;
+  }
+
+  if (!q.value) {
+    q.classList.add('is-invalid');
+    erroreQ.innerText = 'Compila questo campo';
+    ret--;
+  } else {
+    q.classList.remove('is-invalid');
+    erroreQ.innerText = '';
+    ret++;
+  }
+
+  return ret;
+};
+
+
+const pulisciCanvas = ctx => {
+  window.location.reload(false);
+};
+
+const disegnaFunzione = (ctx, m, q) => {
+  /* y = mx + q */
+  ctx.strokeStyle = '#FF0000';
+  ctx.scale(1, -1);
+  ctx.beginPath();
+  ctx.moveTo(-200, -200);
+  for (let x = -200; x < 200; x++) {
+    console.log(`x: ${ x }, y:${ (m * x) + q }`);
+    ctx.lineTo(x, m * x + q);
+    ctx.stroke();
+  }
+};
+
+const disegnaTestoSuCartesiano = (ctx, x, y, text) => {
+  ctx.fillText(text, x, -(y));
+};
+
+const disegnaCanvas = () => {
+    if (validaCampi() < 0)
+      return -1;
+
+
+    console.log('disegno...');
+
+    const c = document.getElementById('cnvGrafico');
+    const ctx = c.getContext('2d');
+
+    ctx.translate(200, 200);
+
+    ctx.beginPath();
+    ctx.moveTo(-200, 0);
+    ctx.lineTo(200, 0);
+    ctx.stroke();
+    ctx.moveTo(0, -200);
+    ctx.lineTo(0, 200);
+    ctx.stroke();
+
+    ctx.font = '12px Arial';
+    ctx.fillText('x', 180, -10);
+
+    ctx.fillText('y', 10, -180);
+
+    ctx.fillRect(-100, -100, 4, 4);
+
+
+    disegnaTestoSuCartesiano(ctx, -200, -200, `m=${ m.value } q=${ q.value }`);
+    disegnaFunzione(ctx, m.value * 1, q.value * 1);
+
+    btnSalva.disabled = false;
+  }
+;
+
+const salvaDati = () => {
+  console.log('salvo...');
+
+  ss.setItem('m', m.value);
+  ss.setItem('q', q.value);
+};
+
+const caricaDati = () => {
+  console.log('carico...');
+
+  m.value = ss.getItem('m');
+  q.value = ss.getItem('q');
+
+  btnCarica.disabled = true;
+};
+
+
+document.getElementById('btnDisegna').addEventListener('click', disegnaCanvas);
+document.getElementById('btnClear').addEventListener('click', pulisciCanvas);
+document.getElementById('btnSalva').addEventListener('click', salvaDati);
+document.getElementById('btnCarica').addEventListener('click', caricaDati);
